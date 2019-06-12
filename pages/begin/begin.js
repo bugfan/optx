20fbec591ff037afbfcd2462d733dcfcf326311e
bugfan <908958194@qq.com>
2019-06-12 16:43:16 +0800

    first

---
+++ pages/begin/begin.js
@@ -0,0 +1,42 @@
+//index.js
+//获取应用实例
+const app = getApp()
+const wordList = require('../../utils/word.js')  //题库
+Page({
+ 
+  practiceStart: function () {
+    
+    
+    wx.redirectTo({   //页面
+      url: '../play/play',
+    })
+  },
+
+  // bindInfo: function () {
+  //   wx.redirectTo({
+  //     url: '../info/info',
+  //   })
+  // },
+  
+  commonStart: function () {
+    wx.redirectTo({
+      url: '../animation/animation',
+    })
+  },
+
+
+  //从后台获取题目
+  getOpts: function () {
+    app.globalData.num = wordList.word
+    console.log("题目:",app.globalData.num)
+  },
+
+  onReady:function(){
+    //生成题目
+    this.getOpts();
+    //清除错题记录
+    app.globalData.numValue.length = 0;
+    
+    
+  }
+})
